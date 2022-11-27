50296ad6b3ce8d7fe765f12e2d73372942cc6eba
Gino Murin <gmurin@gmail.com>
2022-11-27 16:31:31 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This is a test to see if the build catches this update
       </p>
     </div>
   )
